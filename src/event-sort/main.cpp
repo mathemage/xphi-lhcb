c2cac731039661ee6dc41d406af32b660a956469
Karel Ha <mathemage@gmail.com>
2015-08-25 17:33:01 +0200

    Create event-sort project
    - only "Hello World" functionality, so far

---
+++ src/event-sort/main.cpp
@@ -0,0 +1,21 @@
+ 
+/* ========================================
+
+   * File Name : main.cpp
+
+   * Creation Date : 25-08-2015
+
+   * Last Modified : Tue 25 Aug 2015 05:17:12 PM CEST
+
+   * Created By : Karel Ha <mathemage@gmail.com>
+
+   ==========================================*/
+
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+  printf("Hello World!\n");
+  return 0;
+}

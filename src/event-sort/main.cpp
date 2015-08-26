6fb33fc81afb8da9b4e12636bd53ccd2c0c621ef
Karel Ha <mathemage@gmail.com>
2015-08-26 17:23:04 +0200

    Import event-sort from prefix-offset
    - rename variables according to new terminology

--- src/event-sort/main.cpp
+++ src/event-sort/main.cpp
@@ -1,21 +1,109 @@
- 
+
 /* ========================================
 
-   * File Name : main.cpp
+ * File Name : main.cpp
 
-   * Creation Date : 25-08-2015
+ * Creation Date : 25-08-2015
 
-   * Last Modified : Tue 25 Aug 2015 05:17:12 PM CEST
+ * Last Modified : Wed 26 Aug 2015 05:21:59 PM CEST
 
-   * Created By : Karel Ha <mathemage@gmail.com>
+ * Created By : Karel Ha <mathemage@gmail.com>
 
-   ==========================================*/
+ ==========================================*/
 
-#include <cstdio>
+#include "commons.h"
+#include "utils.h"
+#include "prefix-sum.h"
 
-using namespace std;
+// #define VERBOSE_MODE
 
-int main() {
-  printf("Hello World!\n");
+int main(int argc, char *argv[]) {
+  /* ------------------------------------------------------------------------ */
+  /* PARSING ARGUMENTS */
+  int opt;
+  size_t mep_factor = 10000;
+  long long iterations = 10;
+  long long sources = 1000;
+  length_t min_length = 80;
+  length_t max_length = 150;
+
+  while ((opt = getopt(argc, argv, "i:m:s:x:n")) != -1) {
+    switch (opt) {
+      case 'i':
+        iterations = get_argument_long_value(optarg, "-i");
+        break;
+      case 'm':
+        mep_factor = get_argument_long_value(optarg, "-m");
+        break;
+      case 's':
+        sources = get_argument_long_value(optarg, "-s");
+        break;
+      case 'x':
+        max_length = get_argument_long_value(optarg, "-x");
+        break;
+      case 'n':
+        min_length = get_argument_long_value(optarg, "-n");
+        break;
+      default:
+        fprintf(stderr, "Usage: %s [-i number_of_iterations] \
+            [-m mep_factor] [-s number_of_sources] \
+            [-x max_length] [-n min_length] \
+            \n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+  }
+  /* ------------------------------------------------------------------------ */
+
+
+  tbb::tick_count start, end;
+  tbb::tick_count::interval_t total_time;
+  /* ------------------------------------------------------------------------ */
+  /* TRANSPOSE OF MEPs */
+  for (long long i = 0; i < iterations; i++) {
+#ifdef VERBOSE_MODE
+    printf("\n---------------------------\n");
+    printf("Iteration #%d\n", i+1);
+#endif
+    length_t *lengths = generate_random_lengths(mep_factor, min_length, max_length);
+    offset_t *read_offsets = (offset_t *) calloc(mep_factor, sizeof(offset_t));
+
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    /* PREFIX OFFSETS FOR READING: EXCLUSIVE SCAN */
+#ifdef VERBOSE_MODE
+    printf("MEP lengths:\n");
+    show_lengths(lengths, mep_factor);
+#endif
+    start = tbb::tick_count::now();
+    prefix_sum_sequential<length_t, offset_t>(lengths, read_offsets, mep_factor, 0);
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+
+#ifdef VERBOSE_MODE
+    printf("Offsets:\n");
+    show_offsets(read_offsets, mep_factor);
+#endif
+
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    /* PREFIX OFFSETS FOR READING: EXCLUSIVE SCAN */
+    // TODO
+    /* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+    
+    end = tbb::tick_count::now();
+    total_time += (end - start);
+    free(read_offsets);
+    free(lengths);
+  }
+  /* ------------------------------------------------------------------------ */
+
+  printf("\n----------SUMMARY----------\n");
+  const long long total_elements = mep_factor * iterations;
+  printf("Total elements: %llu\n", total_elements);
+
+  printf("Total time: %g secs\n", total_time.seconds());
+  printf("Processed: %g elements per second\n", total_elements / total_time.seconds());
+
+  const unsigned long long bytes_in_gb = 1000000000;
+  const unsigned long long total_size = total_elements * sizeof(length_t) / bytes_in_gb;
+  printf("Processed: %g GBps\n", total_size / total_time.seconds());
+  printf("---------------------------\n");
   return 0;
 }

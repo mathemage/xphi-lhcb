0836c89706a5ac9b0e285dbd23db6a5f7daab19f
Karel Ha <mathemage@gmail.com>
2015-12-03 11:51:06 +0100

    Execute scan_blocks_params() directly

--- src/event-sort/problem-sizes-benchmarks.sh
+++ src/event-sort/problem-sizes-benchmarks.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-niters=10
+niters=100
 
 function scan_block_params {
   srange=`seq 2 32`
@@ -9,17 +9,18 @@ function scan_block_params {
   for sblock in $srange; do
     for mblock in $mrange; do
       cmd="./event-sort.knc.exe -q -1 $sblock -2 $mblock -s $1 -m $2 -i $niters"
-      #echo $cmd >/dev/stdout
+      #echo $cmd
       $cmd
     done
     echo
   done
 }
 
+directory="./results/problem-sizes-variation/$niters-iterations"
+mkdir $directory -p
+
 for i in `seq 10`; do
   sources=$((100*$i))
   mep_factor=$((1000*$i))
-  cmd="scan_block_params $sources $mep_factor | tee $sources-$mep_factor.dat"
-  echo $cmd
-  $cmd
+  scan_block_params $sources $mep_factor | tee "$directory/$sources-$mep_factor.dat"
 done

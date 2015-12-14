1fa6c78a3ec30bbed6e5dcc8a8775bb1e20016f9
Karel Ha <mathemage@gmail.com>
2015-12-14 15:34:53 +0100

    Fix z-axis range of all heatmaps

--- src/event-sort/results/problem-sizes-variation/100-iterations/1000-sources/heatmap.sh
+++ src/event-sort/results/problem-sizes-variation/100-iterations/1000-sources/heatmap.sh
@@ -13,6 +13,7 @@ for i in `seq 10`; do
     set ylabel "Sources per block"
     set xrange [4:32]
     set yrange [2:32]
+    set cbrange [10:30]
 
     set title "Throughputs for various dimensions of blocks (in GB/s), $sources sources, MEP factor of $events, $niters iterations"
     plot "./$sources-$events.dat" u (\$1+4):(\$2+2):3 matrix w image notitle
@@ -31,6 +32,7 @@ for i in `seq 10`; do
     set ylabel "Sources per block"
     set xrange [4:32]
     set yrange [2:32]
+    set cbrange [10:30]
 
     set title "Throughputs for various dimensions of blocks (in GB/s), $sources sources, MEP factor of $events, $niters iterations"
     plot "./$sources-$events.dat" u (\$1+4):(\$2+2):3 matrix w image notitle

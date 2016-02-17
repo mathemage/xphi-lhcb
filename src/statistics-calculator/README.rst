0c73c66064669a9928235d247c25218d2c0b3bc0
Karel Ha <mathemage@gmail.com>
2016-02-17 11:06:42 +0100

    Create statistics-calculator tool

---
+++ src/statistics-calculator/README.rst
@@ -0,0 +1,17 @@
+==========
+EVENT SORT
+==========
+
+:Author: Karel Ha
+:Contact: karel.ha@cern.ch, mathemage@gmail.com
+:Created on: $Date: Wed Feb 17 2016 $
+
+About
+-----
+
+This program calculates some statistics for the stream of real numbers. Namely
+
+- mean (arithmetic average)
+- min
+- max
+- standard deviation
---
+++ src/statistics-calculator/main.cpp
@@ -0,0 +1,19 @@
+ 
+/* ========================================
+
+   * File Name : main.cpp
+
+   * Creation Date : 17-02-2016
+
+   * Last Modified : Wed 17 Feb 2016 11:03:39 AM CET
+
+   * Created By : Karel Ha <mathemage@gmail.com>
+
+   ==========================================*/
+
+#include <cstdio>
+
+int main() {
+  printf("Hello Priya!\n");
+  return 0;
+}

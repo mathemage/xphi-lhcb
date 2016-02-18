071ca7e3e133c0e3940d172e52add651cea22640
Karel Ha <mathemage@gmail.com>
2016-02-18 17:28:36 +0100

    Add missing decimal "." in the regex of extract-numbers.sh

--- src/event-sort/results/extract-numbers.sh
+++ src/event-sort/results/extract-numbers.sh
@@ -4,5 +4,5 @@ for mic_num in 0 1 2
 do
   base="runs-with-1-iteration-MIC$mic_num-28.30"
   keyword="mean"
-	grep $keyword $base.results.out | sed -s 's/[^0-9 ]*//g' >$base.numbers.out
+	grep $keyword $base.results.out | sed -s 's/[^0-9 \.]*//g' >$base.numbers.out
 done

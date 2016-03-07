e687a95a5f256ef2f298bd6b01d7cb408d5554a0
Karel Ha <mathemage@gmail.com>
2016-03-07 12:44:04 +0100

    Rename vary-copy-threads folders according to their ranges

--- results/event-sort/custom-seed/vary-copy-threads-exp-range/nthreads-vs-stddev.sh
+++ results/event-sort/custom-seed/vary-copy-threads-exp-range/nthreads-vs-stddev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 seeds="0 83 89"
-#ymax="0.28"
+ymax="0.28"
 
 for seed in $seeds
 do
@@ -13,7 +13,7 @@ do
   set xrange [1:64]
   set xtics (1,2,4,8,16,32,64)
   set ylabel "(Sample) standard deviation per 10 experiments (in GB/s)"
-  #set yrange [0:$ymax]
+  set yrange [0:$ymax]
   set title "Standard deviation (in GB/s) for various number of threads, srand() initialized with seed $seed"
   filename(n)=sprintf("seed-$seed-MIC-%d.dat", n)
   plot for [m=0:2] filename(m) with linespoints title sprintf("kernel.randomize_va_space = %d", m)

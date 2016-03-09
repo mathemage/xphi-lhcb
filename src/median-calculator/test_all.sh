994b4b25e810783cb0f96ecb4c767a92b6fd3ec6
Karel Ha <mathemage@gmail.com>
2016-03-09 11:33:17 +0100

    Test uncertainty feature in test_all.sh

--- src/median-calculator/test_all.sh
+++ src/median-calculator/test_all.sh
@@ -3,7 +3,20 @@
 make clean
 make median-calculator-icpc
 
+echo "################################################################################"
+echo "                               Medians                                          "
 for f in *.in
 do
   echo "Median of $(cat $f) == $(./median-calculator-icpc <$f)"
 done
+echo "################################################################################"
+
+echo
+
+echo "################################################################################"
+echo "                             Uncertainties                                      "
+for f in *.in
+do
+  echo "Uncertainty of $(cat $f) == $(./median-calculator-icpc --uncertainty <$f)"
+done
+echo "################################################################################"
